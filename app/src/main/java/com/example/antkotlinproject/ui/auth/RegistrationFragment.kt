a200d1a1822d9a4ee009bf35c4eff0950cbb937a
emikoo <emikoo@users.noreply.github.com>
2021-06-09 20:22:03 +0600

    added_layout for registration list

--- app/src/main/java/com/example/antkotlinproject/ui/auth/RegistrationFragment.kt
+++ app/src/main/java/com/example/antkotlinproject/ui/auth/RegistrationFragment.kt
@@ -3,4 +3,4 @@ package com.example.antkotlinproject.ui.auth
 import com.example.antkotlinproject.R
 import com.example.antkotlinproject.ui.base.BaseFragment
 
-class CheckUserRegistrationFragment : BaseFragment(R.layout.fragment_check_user_registration)
+class RegistrationFragment : BaseFragment(R.layout.fragment_registration)
---
+++ app/src/main/java/com/example/antkotlinproject/ui/base/BaseActivity.kt
@@ -0,0 +1,6 @@
+package com.example.antkotlinproject.ui.base
+
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class BaseActivity: AppCompatActivity() {
+}
