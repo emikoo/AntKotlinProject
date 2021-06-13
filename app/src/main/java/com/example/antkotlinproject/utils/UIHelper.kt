c34b50fd3f7ab8550a23eb1a62dd9bbdcf2b864e
emikoo <emikoo@users.noreply.github.com>
2021-06-13 19:45:52 +0600

    added ui helper with show snackbar and toast methods

---
+++ app/src/main/java/com/example/antkotlinproject/utils/UIHelper.kt
@@ -0,0 +1,29 @@
+package com.example.antkotlinproject.utils
+
+import android.content.Context
+import android.view.View
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import com.example.antkotlinproject.R
+import com.google.android.material.snackbar.Snackbar
+
+fun showActionSnackbar(
+    view: View,
+    message: String,
+    actionTitle: String,
+    action: () -> Unit,
+    context: Context
+) {
+    Snackbar.make(view, message, Snackbar.LENGTH_LONG).setAction(actionTitle) {
+        action()
+    }.setActionTextColor(ContextCompat.getColor(context, R.color.color_dark_green)).show()
+}
+
+var toast: Toast? = null
+fun showToast(context: Context, message: String) {
+    if (toast != null) toast?.cancel()
+    toast = Toast.makeText(context, message, Toast.LENGTH_LONG)
+    toast?.show()
+}
+
+
