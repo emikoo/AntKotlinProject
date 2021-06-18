2f8feb9784f67b864b42d3521a7244d78afe182e
emikoo <emikoo@users.noreply.github.com>
2021-06-18 20:48:15 +0600

    added login action

--- app/src/main/java/com/example/antkotlinproject/ui/auth/login/LoginFragment.kt
+++ app/src/main/java/com/example/antkotlinproject/ui/auth/login/LoginFragment.kt
@@ -1,29 +1,55 @@
 package com.example.antkotlinproject.ui.auth.login
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
-import com.example.antkotlinproject.databinding.FragmentLoginBinding
-import com.example.antkotlinproject.ui.auth.AuthorizationViewModel
 import com.example.antkotlinproject.base.BaseFragment
+import com.example.antkotlinproject.databinding.FragmentLoginBinding
+import com.example.antkotlinproject.ui.auth.AuthViewModel
+import com.example.antkotlinproject.ui.user.main.MainUserActivity
+import org.koin.androidx.viewmodel.ext.android.getViewModel
 
-class LoginFragment : BaseFragment<AuthorizationViewModel, FragmentLoginBinding>(
-    AuthorizationViewModel::class
+class LoginFragment : BaseFragment<AuthViewModel, FragmentLoginBinding>(
+    AuthViewModel::class
 ) {
     override fun attachBinding(
         list: MutableList<FragmentLoginBinding>,
         layoutInflater: LayoutInflater,
         container: ViewGroup?,
-        attachToRoot: Boolean) {
+        attachToRoot: Boolean
+    ) {
         list.add(FragmentLoginBinding.inflate(layoutInflater, container, attachToRoot))
     }
 
     override fun setupViews() {
+        viewModel = getViewModel(clazz = AuthViewModel::class)
         setupListener()
+        setupViewModel()
     }
 
     private fun setupListener() {
         binding.btnNegative.setOnClickListener { findNavController().popBackStack() }
+        binding.btnPositive.setOnClickListener { login() }
+    }
+
+    private fun login() {
+        viewModel.login(
+            username = binding.etLogin.text.toString(),
+            password = binding.etPassword.text.toString()
+        )
+    }
+
+    private fun setupViewModel() {
+        viewModel.actionNewScreen.observe(requireActivity(), Observer{
+            if (it == true) startActivity(Intent(requireContext(), MainUserActivity::class.java))
+            activity?.finish()
+        })
+        viewModel.error.observe(requireActivity(), Observer{
+            Toast.makeText(requireContext(), it, Toast.LENGTH_LONG).show()
+        })
     }
 
     override fun subscribeToLiveData() {}
