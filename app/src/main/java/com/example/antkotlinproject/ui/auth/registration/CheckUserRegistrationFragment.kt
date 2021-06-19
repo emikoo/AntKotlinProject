73a13adecd9c40f4269e71d0d4c441ee802b8c37
emikoo <emikoo@users.noreply.github.com>
2021-06-19 23:04:44 +0600

    change state of btn when stuff checked

--- app/src/main/java/com/example/antkotlinproject/ui/auth/registration/CheckUserRegistrationFragment.kt
+++ app/src/main/java/com/example/antkotlinproject/ui/auth/registration/CheckUserRegistrationFragment.kt
@@ -2,15 +2,21 @@ package com.example.antkotlinproject.ui.auth.registration
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.navigation.fragment.findNavController
 import com.example.antkotlinproject.R
 import com.example.antkotlinproject.databinding.FragmentCheckUserRegistrationBinding
 import com.example.antkotlinproject.ui.auth.AuthorizationViewModel
 import com.example.antkotlinproject.base.BaseFragment
+import com.example.antkotlinproject.utils.showToast
 
 class CheckUserRegistrationFragment :
     BaseFragment<AuthorizationViewModel, FragmentCheckUserRegistrationBinding>(
         AuthorizationViewModel::class) {
+
+    private var isPupilClicked: Boolean = false
+    private var isTeacherClicked: Boolean = false
+    private var isStuff: Boolean =  false
 
     override fun attachBinding(
         list: MutableList<FragmentCheckUserRegistrationBinding>,
@@ -25,10 +31,40 @@ class CheckUserRegistrationFragment :
     }
 
     private fun setupListeners() {
-        binding.btnPositive.setOnClickListener {
-            findNavController().navigate(R.id.action_checkUserRegistrationFragment_to_registrationFragment2)
+        binding.btnPupil.setOnClickListener {
+            isPupilClicked = !isPupilClicked
+            isTeacherClicked = false
+            isChecked()
+        }
+        binding.btnTeacher.setOnClickListener {
+            isTeacherClicked = !isTeacherClicked
+            isPupilClicked = false
+            isChecked()
         }
         binding.btnNegative.setOnClickListener { findNavController().popBackStack() }
+        binding.btnPositive.setOnClickListener {
+            if (isTeacherClicked || isPupilClicked) {
+                isStuff = isTeacherClicked
+                argAction(isStuff)
+            }
+            else { showToast(requireContext(), "Выберите один вариант") }
+        }
+    }
+
+    private fun isChecked() {
+        changeCheckedState(isTeacherClicked, binding.btnTeacher)
+        changeCheckedState(isPupilClicked, binding.btnPupil)
+    }
+
+    private fun changeCheckedState(flag: Boolean, button: Button) {
+        if (flag){
+            button.setBackgroundColor(resources.getColor(R.color.color_green))
+            button.setTextColor(resources.getColor(R.color.color_bej))
+        } else {
+            button.setBackgroundColor(resources.getColor(R.color.color_bej))
+            button.setTextColor(resources.getColor(R.color.color_green))
+        }
+    }
     }
 
     override fun subscribeToLiveData() {}
