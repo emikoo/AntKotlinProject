b86d889e4a7661b1d18a5bf532e14d7445553638
emikoo <emikoo@users.noreply.github.com>
2021-06-19 23:06:39 +0600

    added cloud server's api constants

--- app/src/main/java/com/example/antkotlinproject/utils/ApiConstants.kt
+++ app/src/main/java/com/example/antkotlinproject/utils/ApiConstants.kt
@@ -1,8 +1,8 @@
 package com.example.antkotlinproject.utils
 
 object ApiConstants {
-    const val BASE_URL = "http://192.168.88.251:8000"
-    const val REGISTER_URL = "/user/signup/"
-    const val LOGIN_TOKEN_URL = "/user/signin/"
-    const val REFRESH_TOKEN_URL = "/user/api/token/refresh/"
+    const val BASE_URL = "http://bobowka.pythonanywhere.com/"
+    const val REGISTER_URL = "user/signup/"
+    const val LOGIN_TOKEN_URL = "user/signin/"
+    const val REFRESH_TOKEN_URL = "user/api/token/refresh/"
 }
