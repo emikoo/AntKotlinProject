3e8250bb390a1b4be3612bc9fda0c9d8fc5ff8c3
emikoo <emikoo@users.noreply.github.com>
2021-06-25 15:31:21 +0600

    api for fetching subcategory array

---
+++ app/src/main/java/com/example/antkotlinproject/ui/user/bottomnavigation/search/categories/ContainerSubCourseAdapter.kt
@@ -0,0 +1,21 @@
+package com.example.antkotlinproject.ui.user.bottomnavigation.search.categories
+
+import android.view.ViewGroup
+import com.example.antkotlinproject.base.BaseAdapter
+import com.example.antkotlinproject.base.BaseViewHolder
+
+
+class ContainerSubCourseAdapter: BaseAdapter() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: BaseViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+}
