6f2fd044c6c5a9b5249a748cbbba4cc6a9f057ff
emikoo <emikoo@users.noreply.github.com>
2021-06-25 15:32:12 +0600

    method in repository for fetching subcategory array

--- app/src/main/java/com/example/antkotlinproject/repository/SearchRepository.kt
+++ app/src/main/java/com/example/antkotlinproject/repository/SearchRepository.kt
@@ -2,6 +2,7 @@ package com.example.antkotlinproject.repository
 
 import com.example.antkotlinproject.data.model.CategoryModel
 import com.example.antkotlinproject.data.model.CourseModel
+import com.example.antkotlinproject.data.model.SubcategoryModel
 import com.example.antkotlinproject.data.network.api.CourseApi
 import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
@@ -11,6 +12,7 @@ interface SearchRepository {
     fun fetchCategory(): Observable<MutableList<CategoryModel>>
     fun fetchCourses(): Observable<MutableList<CourseModel>>
     fun fetchCourse(id: Int): Observable<CourseModel>
+    fun fetchSubcategory(categoryId: Int): Observable<CategoryModel>
 }
 
 class SearchRepositoryImpl(private val api: CourseApi) : SearchRepository {
@@ -28,6 +30,12 @@ class SearchRepositoryImpl(private val api: CourseApi) : SearchRepository {
 
     override fun fetchCourse(id: Int): Observable<CourseModel> {
         return api.fetchCourse(id)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+    }
+
+    override fun fetchSubcategory(categoryId: Int): Observable<CategoryModel> {
+        return api.fetchSubcategory(categoryId)
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
     }
