67a5dcec802bc1abab8c8ce96818353adb21fe44
emikoo <emikoo@users.noreply.github.com>
2021-07-01 19:58:41 +0600

    added base fragment with image uploading

--- app/src/main/java/com/example/antkotlinproject/base/BaseFragment.kt
+++ app/src/main/java/com/example/antkotlinproject/base/BaseFragment.kt
@@ -1,16 +1,11 @@
 package com.example.antkotlinproject.base
 
-import android.app.Activity
-import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import androidx.viewbinding.ViewBinding
-import com.bumptech.glide.Glide
 import org.koin.androidx.viewmodel.ext.android.getViewModel
 import kotlin.reflect.KClass
 
@@ -52,33 +47,6 @@ abstract class BaseFragment<ViewModel : BaseViewModel<BaseEvent>, VB_CHILD : Vie
 
     private fun init() {
         getViewModel(clazz = clazz)
-    }
-
-    fun selectPicture() {
-        val i = Intent()
-        i.type = "image/*"
-        i.action = Intent.ACTION_GET_CONTENT
-        startActivityForResult(
-            Intent.createChooser(i, "Select Picture"),
-            SELECT_PICTURE
-        )
-    }
-
-    fun getSelectedPicture(resultCode: Int, requestCode: Int, data: Intent?, imageView: ImageView) {
-        if (resultCode == Activity.RESULT_OK) {
-            if (requestCode == SELECT_PICTURE) {
-                val selectedImageUri: Uri? = data?.data
-                if (null != selectedImageUri) {
-                    Glide.with(imageView)
-                        .load(selectedImageUri)
-                        .into(imageView)
-                }
-            }
-        }
-    }
-
-    companion object {
-        const val SELECT_PICTURE = 200
     }
 
     override fun onDestroy() {
